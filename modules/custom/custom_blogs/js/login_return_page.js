e893f15a2eb65454584d971b8c51ff7d9bdc609b
Eliot Scott <eliotwsc@umich.edu>
2022-11-07 13:01:48 -0500

    alter add member blurb and add blogs-signup destination

--- modules/custom/custom_blogs/js/login_return_page.js
+++ modules/custom/custom_blogs/js/login_return_page.js
@@ -5,9 +5,13 @@
   Drupal.behaviors.linkReturnPage = {
     attach: function (context) {
       var current_page = location.pathname,
-      login_page = '/user/login';
+      login_page = '/user/login',
+      signup_page = '/blogs-signup';
 
       if (login_page != current_page) {
+        if (signup_page == current_page) {
+          current_page = location.search.replace("?destination=", "");
+        }
         $(context).find("a[href='" + login_page + "']").once('login-processed-link').each(function () {
           $(this).attr('href', $(this).attr('href') + '?destination=' + current_page);
         });
